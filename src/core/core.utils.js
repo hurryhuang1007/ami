ef93f4a362a296e82d472689fb2fc66f89216dcb
hurryhuang1007 <hurryhuang1007@qq.com>
2018-12-21 12:42:40 +0800

    let ijk and lps be float

--- src/core/core.utils.js
+++ src/core/core.utils.js
@@ -255,11 +255,13 @@ export default class CoreUtils {
     let dataCoordinate = new Vector3().copy(worldCoordinates).applyMatrix4(lps2IJK);
 
     // same rounding in the shaders
-    dataCoordinate.addScalar(0.5).floor();
-    // dataCoordinate.x = Math.floor(dataCoordinate.x * 10) / 10;
-    // dataCoordinate.y = Math.floor(dataCoordinate.y * 10) / 10;
-    // dataCoordinate.z = Math.floor(dataCoordinate.z * 10) / 10;
+    // dataCoordinate.addScalar(0.5).floor();
+    dataCoordinate.addScalar(0.00005);
+    dataCoordinate.x = ~~(dataCoordinate.x * 10000) / 10000;
+    dataCoordinate.y = ~~(dataCoordinate.y * 10000) / 10000;
+    dataCoordinate.z = ~~(dataCoordinate.z * 10000) / 10000;
 
+    // console.log(dataCoordinate)
     return dataCoordinate;
   }
 
